38466fd8ad1fb3a3f24aa76d47999e1f32effe13
myu <myujiku.music@gmail.com>
2022-04-07 09:18:38 +0200

    made things look better in full screen

--- frontend/abstract_menu.py
+++ frontend/abstract_menu.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import (QWidget, QVBoxLayout, QPushButton)
+from PySide6.QtWidgets import (QWidget, QVBoxLayout, QPushButton, QSizePolicy)
 
 from frontend.window_manager import WindowManager
 
@@ -12,11 +12,20 @@ class AbstractMenu(QWidget):
     def setup(self) -> None:
         self.root_layout = QVBoxLayout(self)
 
+        self.root_layout.addWidget(QWidget())
+        self.root_layout.addWidget(QWidget())
+
+        self.buttons: int = 0
+
     def init_buttons(self) -> None:
         pass
 
     def add_button(self, text: str, function: callable = None) -> None:
         button = QPushButton(text=text)
+        button.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        button.setMaximumSize(16777215, 100)
         button.clicked.connect(function)
-        self.root_layout.addWidget(button)
+
+        self.buttons += 1
+        self.root_layout.insertWidget(self.buttons, button)
 
--- frontend/draft_options_window.py
+++ frontend/draft_options_window.py
@@ -1,5 +1,4 @@
-
-
+from PySide6.QtCore import (QSize)
 from PySide6.QtWidgets import (QWidget, QGridLayout, QHBoxLayout, QSizePolicy, QLabel, QFrame, QSpinBox, QPushButton)
 
 from backend import settings
@@ -8,7 +7,7 @@ from frontend.round_draft_window import RoundDraftWindow
 from frontend.set_selection_window import SetSelectionWindow
 
 
-class OptionBox(QWidget):
+class OptionBox(QFrame):
     def __init__(self, text: str, max_value: int, min_value: int = 1) -> None:
         self.label_text: str = text
         self.max_value: int = max_value
@@ -18,16 +17,18 @@ class OptionBox(QWidget):
         self.setup_ui()
 
     def setup_ui(self) -> None:
+        self.setFrameStyle(QFrame.StyledPanel | QFrame.Raised)
+
         self.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Maximum)
 
         self.main_layout = QHBoxLayout(self)
 
         self.text_label = QLabel(text=self.label_text)
-        self.text_label.setFrameStyle(QFrame.StyledPanel | QFrame.Sunken)
+        self.text_label.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Minimum)
         self.main_layout.addWidget(self.text_label)
 
         self.spin_box = QSpinBox()
-        self.spin_box.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Preferred)
+        self.spin_box.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
         self.spin_box.setMaximum(self.max_value)
         self.spin_box.setMinimum(self.min_value)
         self.spin_box.setMinimumSize(55, 0)
@@ -50,6 +51,7 @@ class DraftOptionsWindow(QWidget):
 
     def init_vars(self) -> None:
         self.selected_sets = []
+        self.layout_position: int = 0
 
     def setup_ui(self) -> None:
         self.grid = QGridLayout(self)
@@ -58,33 +60,52 @@ class DraftOptionsWindow(QWidget):
 
         self.draft_rounds_box = OptionBox("Draft Rounds", 300)
         self.draft_rounds_box.setValue(defaults["draft_rounds"])
+        self.set_size(self.draft_rounds_box)
 
         self.card_bundles_box = OptionBox("Card Bundles/Choices", 50)
         self.card_bundles_box.setValue(defaults["card_bundles"])
+        self.set_size(self.card_bundles_box)
 
         self.cards_per_bundle_box = OptionBox("Cards per Bundle", 50)
         self.cards_per_bundle_box.setValue(defaults["cards_per_bundle"])
+        self.set_size(self.cards_per_bundle_box)
 
         self.duplicates_box = OptionBox("Duplicates in Card Pool", 10)
         self.duplicates_box.setValue(defaults["duplicates_in_pool"])
+        self.set_size(self.duplicates_box)
 
         self.select_sets_button = QPushButton(text="Select Sets")
+        self.set_size(self.select_sets_button)
         self.select_sets_button.clicked.connect(self.set_selection)
 
         self.view_contents_button = QPushButton(text="View Contents")
-        
+        self.set_size(self.view_contents_button)
 
         self.start_draft_button = QPushButton(text="Start Draft")
+        self.set_size(self.start_draft_button)
         self.start_draft_button.clicked.connect(self.start_draft)
         self.start_draft_button.setEnabled(False)
 
-        self.grid.addWidget(self.draft_rounds_box, 0, 0)
-        self.grid.addWidget(self.card_bundles_box, 0, 1)
-        self.grid.addWidget(self.duplicates_box, 1, 0)
-        self.grid.addWidget(self.cards_per_bundle_box, 1, 1)
-        self.grid.addWidget(self.select_sets_button, 2, 0)
-        self.grid.addWidget(self.view_contents_button, 2, 1)
-        self.grid.addWidget(self.start_draft_button, 3, 0, 1, 2)
+        self.add_to_layout(QWidget())
+        self.add_to_layout(self.draft_rounds_box, self.card_bundles_box)
+        self.add_to_layout(self.duplicates_box, self.cards_per_bundle_box)
+        self.add_to_layout(QWidget())
+        self.add_to_layout(self.select_sets_button, self.view_contents_button)
+        self.add_to_layout(self.start_draft_button)
+        self.add_to_layout(QWidget())
+
+    def set_size(self, widget: QWidget) -> None:
+        widget.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        widget.setMaximumSize(QSize(16777215, 100))
+
+    def add_to_layout(self, widget1: QWidget, widget2: QWidget = None) -> None:
+        if widget2:
+            self.grid.addWidget(widget1, self.layout_position, 0)
+            self.grid.addWidget(widget2, self.layout_position, 1)
+        else:
+            self.grid.addWidget(widget1, self.layout_position, 0, 1, 2)
+
+        self.layout_position += 1
 
     def set_selection(self) -> None:
         parent = self.parent()
