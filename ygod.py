0369770f9e9bb88abc9e32d3097ae199c5333c2e
myu <myujiku.music@gmail.com>
2022-04-07 22:06:48 +0200

    testing stuff

--- ygod.py
+++ ygod.py
@@ -1,4 +1,6 @@
-from PySide6.QtWidgets import (QApplication)
+from PySide6.QtCore import (QSize, Qt)
+from PySide6.QtGui import (QAction, QIcon)
+from PySide6.QtWidgets import (QApplication, QMainWindow, QToolBar)
 
 from backend import card_images
 from backend import collection
@@ -21,9 +23,38 @@ if __name__ == "__main__":
             self.add_button("Draft", lambda: self.parent().open_window(DraftOptionsWindow()))
             self.add_button("Update", database.update)
             self.add_button("Download All Images", card_images.download_all)
+            from frontend.edit_collections_menu import EditCollectionsMenu
+
+            parent = self.parent()
+            def open_collections_menu(replace_window: bool = False):
+                nonlocal parent
+                if not parent:
+                    parent = self.parent()
+                collections_menu = EditCollectionsMenu()
+                collections_menu.collections_changed.connect(lambda: open_collections_menu(True))
+                [parent.open_window, parent.replace_window][replace_window](collections_menu)
+
+            self.add_button("test", open_collections_menu)
 
     window_manager = WindowManager(MainMenu)
 
-    window_manager.show()
+    main_window = QMainWindow()
+    main_window.setCentralWidget(window_manager)
+    main_window.resize(800, 600)
+
+    tool_bar = QToolBar()
+    tool_bar.setMovable(False)
+    tool_bar.setContextMenuPolicy(Qt.PreventContextMenu)
+
+    go_back_action = QAction()
+    back_icon = QIcon()
+    back_icon.addFile("images/ui/arrow_back.svgz", QSize(), QIcon.Normal, QIcon.Off)
+    go_back_action.setIcon(back_icon)
+    go_back_action.triggered.connect(lambda: window_manager.go_back())
+    tool_bar.addAction(go_back_action)
+
+    main_window.addToolBar(tool_bar)
+
+    main_window.show()
     app.exec()
 
