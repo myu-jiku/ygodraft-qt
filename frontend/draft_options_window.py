0dc2894fdfdb343f3c3071539b59af817ca32f68
myu <myujiku.music@gmail.com>
2022-04-12 11:14:54 +0200

    very whacky implementation of requirements for starting drafts

--- frontend/draft_options_window.py
+++ frontend/draft_options_window.py
@@ -18,6 +18,7 @@ class OptionBox(QFrame):
 
         super().__init__()
         self.setup_ui()
+        self.value_changed = self.spin_box.valueChanged
 
     def setup_ui(self) -> None:
         self.setFrameStyle(QFrame.StyledPanel | QFrame.Raised)
@@ -55,6 +56,8 @@ class DraftOptionsWindow(QWidget):
     def init_vars(self) -> None:
         self.cards_in_sets: list = []
         self.layout_position: int = 0
+        self.number_of_cards: int = 0
+        self.total_cards: int = 0
 
     def setup_ui(self) -> None:
         self.grid = QGridLayout(self)
@@ -63,19 +66,36 @@ class DraftOptionsWindow(QWidget):
 
         self.draft_rounds_box = OptionBox("Draft Rounds", 200)
         self.draft_rounds_box.setValue(defaults["draft_rounds"])
+        self.draft_rounds_box.value_changed.connect(self.update_labels)
         self.set_size(self.draft_rounds_box)
 
-        self.card_bundles_box = OptionBox("Card Bundles/Choices", 20)
+        self.card_bundles_box = OptionBox("Card Bundles", 20)
         self.card_bundles_box.setValue(defaults["card_bundles"])
+        self.card_bundles_box.value_changed.connect(self.update_labels)
         self.set_size(self.card_bundles_box)
 
         self.cards_per_bundle_box = OptionBox("Cards per Bundle", 30)
         self.cards_per_bundle_box.setValue(defaults["cards_per_bundle"])
+        self.cards_per_bundle_box.value_changed.connect(self.update_labels)
         self.set_size(self.cards_per_bundle_box)
 
         self.duplicates_box = OptionBox("Duplicates in Card Pool", 10)
         self.duplicates_box.setValue(defaults["duplicates_in_pool"])
+        self.duplicates_box.value_changed.connect(self.update_labels)
         self.set_size(self.duplicates_box)
+
+        self.total_cards = self.number_of_cards * self.duplicates_box.value()
+        self.total_cards_label = QLabel(text=f"Total Cards in Pool: {self.total_cards}")
+        self.set_size(self.total_cards_label)
+
+        self.cards_obtained: int = self.draft_rounds_box.value() * self.cards_per_bundle_box.value()
+        self.generated_cards: int = self.cards_obtained * self.card_bundles_box.value()
+        self.generated_cards_label = QLabel(text=f"Cards Generated from Pool: {self.generated_cards} ({self.total_cards / self.generated_cards * 100}%)")
+        self.set_size(self.generated_cards_label)
+
+        self.cards_obtained_label = QLabel()
+        self.cards_obtained_label.setText(f"Cards Obtained: {self.cards_obtained}")
+        self.set_size(self.cards_obtained_label)
 
         self.select_sets_button = QPushButton(text="Select Sets")
         self.set_size(self.select_sets_button)
@@ -92,10 +112,26 @@ class DraftOptionsWindow(QWidget):
         self.add_to_layout(QWidget())
         self.add_to_layout(self.draft_rounds_box, self.card_bundles_box)
         self.add_to_layout(self.duplicates_box, self.cards_per_bundle_box)
+        self.add_to_layout(self.total_cards_label, self.generated_cards_label)
+        self.add_to_layout(self.cards_obtained_label)
         self.add_to_layout(QWidget())
         self.add_to_layout(self.select_sets_button, self.view_contents_button)
         self.add_to_layout(self.start_draft_button)
         self.add_to_layout(QWidget())
+
+    def update_labels(self) -> None:
+        self.total_cards = self.number_of_cards * self.duplicates_box.value()
+        self.total_cards_label.setText(f"Total Cards in Pool: {self.total_cards}")
+
+        self.cards_obtained: int = self.draft_rounds_box.value() * self.cards_per_bundle_box.value()
+        self.generated_cards: int = self.cards_obtained * self.card_bundles_box.value()
+        percentage: float = self.generated_cards / self.total_cards * 100 if self.total_cards else 0.0
+        self.generated_cards_label.setText(f"Cards Generated from Pool: {self.generated_cards} ({percentage:.1f}%)")
+
+        self.cards_obtained_label.setText(f"Cards Obtained: {self.cards_obtained}")
+
+        self.start_draft_button.setEnabled(self.selected_sets and percentage <= 100)
+
 
     def set_size(self, widget: QWidget) -> None:
         widget.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
@@ -119,9 +155,13 @@ class DraftOptionsWindow(QWidget):
             
             if self.selected_sets and self.selected_sets != selected_sets:
                 self.start_draft_button.setEnabled(True)
-                self.cards_in_sets = database.get_cards_from_sets(self.selected_sets)
+                self.cards_in_sets: list = database.get_cards_from_sets(self.selected_sets)
             elif not self.selected_sets:
                 self.start_draft_button.setEnabled(False)
+                self.cards_in_sets = []
+
+            self.number_of_cards: int = len(self.cards_in_sets)
+            self.update_labels()
 
             parent.go_back()
 
